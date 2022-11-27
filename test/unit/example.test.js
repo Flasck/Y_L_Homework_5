471ebf5d8cf09123aea78038818e9355405fb5c2
Flasck <po_6706@mail.ru>
2022-11-28 01:00:43 +0300

    changed

--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -1,5 +1,8 @@
-describe('Simple Test Case', () => {
-    it('Should return 4', () => {
-        expect(2 + 2).toBe(4);
-    });
-});
+import { render, screen } from "react-scripts-test"
+import Contacts from "../../src/client/pages/Contacts"
+
+test("renders learn react link", () => {
+	render(Contacts)
+	const linkElement = screen.getByText(/Contacts/i)
+	expect(linkElement).toBeInTheDocument()
+})
